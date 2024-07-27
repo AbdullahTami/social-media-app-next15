c5d37e93d1e839360f006410054c0debec775900
abdullahqayid <3boo0odq0v@gmail.com>
2024-07-27 21:45:21 +0300

    triggering infinite loading on scroll bottom done

--- src/app/(main)/ForYouFeed.tsx
+++ src/app/(main)/ForYouFeed.tsx
@@ -7,6 +7,8 @@ import Post from "@/components/posts/Post";
 import React from "react";
 import kyInstance from "@/lib/ky";
 import { Button } from "@/components/ui/button";
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
 
 export default function ForYouFeed() {
   const {
@@ -32,8 +34,16 @@ export default function ForYouFeed() {
 
   const posts = data?.pages.flatMap((page) => page.posts) || [];
   if (status === "pending") {
-    return <Loader2 className="mx-auto animate-spin" />;
+    return <PostsLoadingSkeleton />;
   }
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        No one has posted anything yet.
+      </p>
+    );
+  }
+
   if (status === "error") {
     return (
       <p className="text-center text-destructive">
@@ -42,11 +52,14 @@ export default function ForYouFeed() {
     );
   }
   return (
-    <div className="space-y-5">
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
-      <Button onClick={() => fetchNextPage()}>Load more</Button>
-    </div>
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
   );
 }
