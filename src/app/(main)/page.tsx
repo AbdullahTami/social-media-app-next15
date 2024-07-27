82cab8e48ae6932c0ecc089b12e22e3ab2e08460
abdullahqayid <3boo0odq0v@gmail.com>
2024-07-27 17:42:05 +0300

    displaying trending topics nav utilizing suspense api & react unstable cache done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import React from "react";
 import Post from "@/components/posts/Post";
 import { postDataInclude } from "@/lib/types";
+import TrendsSidebar from "@/components/TrendsSidebar";
 
 export default async function Home() {
   const posts = await prisma.post.findMany({
@@ -10,13 +11,14 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main className="w-full min-w-0">
+    <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
