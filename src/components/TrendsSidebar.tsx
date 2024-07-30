06238f52615d9b49a04c90352271f86f75e53886
abdullahqayid <3boo0odq0v@gmail.com>
2024-07-30 18:36:56 +0300

    implementing initial follow functionality done

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,6 +1,5 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import React, { Suspense } from "react";
@@ -9,6 +8,8 @@ import { Button } from "./ui/button";
 import { unstable_cache } from "next/cache";
 import { bigint } from "zod";
 import { formatNumber } from "@/lib/utils";
+import FollowButton from "./FollowButton";
+import { getUserDataSelect } from "@/lib/types";
 
 export default function TrendsSidebar() {
   return (
@@ -31,8 +32,13 @@ async function WhoToFollow() {
       NOT: {
         id: user.id,
       },
+      followers: {
+        none: {
+          followerId: user.id,
+        },
+      },
     },
-    select: userDataSelect,
+    select: getUserDataSelect(user.id),
     take: 5,
   });
 
@@ -58,7 +64,15 @@ async function WhoToFollow() {
                 </p>
               </div>
             </Link>
-            <Button>Follow</Button>
+            <FollowButton
+              userId={user.id}
+              initialState={{
+                followers: user._count.followers,
+                isFollowedByUser: user.followers.some(
+                  ({ followerId }) => followerId === user.id
+                ),
+              }}
+            />
           </div>
         ))}
       </div>
--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -2,7 +2,7 @@
 
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+import { getPostDataInclude } from "@/lib/types";
 import { createPostSchema } from "@/lib/validation";
 
 export async function submitPostAction(input: string) {
@@ -17,7 +17,7 @@ export async function submitPostAction(input: string) {
       content,
       userId: user.id,
     },
-    include: postDataInclude,
+    include: getPostDataInclude(user.id),
   });
   return newPost;
 }
