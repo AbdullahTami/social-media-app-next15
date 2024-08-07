6ebe7260fdb1ffbec1bd2a906f243c1720d3c6d2
abdullahqayid <3boo0odq0v@gmail.com>
2024-08-07 19:28:55 +0300

    ccommit submit server action done

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -33,5 +33,5 @@ export const updateUserProfileSchema = z.object({
 export type UpdateUserProfileValues = z.infer<typeof updateUserProfileSchema>;
 
 export const createCommentSchema = z.object({
-  comments: requiredString,
+  content: requiredString,
 });
