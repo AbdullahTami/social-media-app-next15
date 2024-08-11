9a9265a70c44fc85596b86698922c6557d07328f
abdullahqayid <3boo0odq0v@gmail.com>
2024-08-11 20:59:47 +0300

    displaying notifications done

---
+++ src/app/(main)/notifications/Notifications.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { NotificationPage } from "@/lib/types";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Loader2 } from "lucide-react";
+
+import React from "react";
+import kyInstance from "@/lib/ky";
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+import Notification from "./Notification";
+
+export default function Notifications() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["Notification"],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get(
+          "/api/notifications",
+          pageParam ? { searchParams: { cursor: pageParam } } : {}
+        )
+        .json<NotificationPage>(),
+
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const notifications = data?.pages.flatMap((page) => page.notifications) || [];
+  if (status === "pending") {
+    return <PostsLoadingSkeleton />;
+  }
+  if (status === "success" && !notifications.length && !hasNextPage) {
+    return (
+      <p className="text-center text-muted-foreground">
+        You don&apos;t have any notification yet.
+      </p>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        An error occurred while loading posts.
+      </p>
+    );
+  }
+  return (
+    <InfiniteScrollContainer
+      className="space-y-5"
+      onBottomReached={() => hasNextPage && !isFetching && fetchNextPage()}
+    >
+      {notifications.map((notification) => (
+        <Notification key={notification.id} notification={notification} />
+      ))}
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+}
