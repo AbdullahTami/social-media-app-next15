0e67f13f6a14ffa8f96362ee62a843392154288d
abdullahqayid <3boo0odq0v@gmail.com>
2024-08-14 22:29:40 +0300

    create new chat dialog done

--- src/app/(main)/messages/ChatSidebar.tsx
+++ src/app/(main)/messages/ChatSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useSession } from "../SessionProvider";
 import {
   ChannelList,
@@ -6,8 +6,9 @@ import {
   ChannelPreviewUIComponentProps,
 } from "stream-chat-react";
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
+import { MailPlus, X } from "lucide-react";
 import { cn } from "@/lib/utils";
+import NewChatDialog from "./NewChatDialog";
 
 interface ChatSidebarProps {
   open: boolean;
@@ -75,13 +76,34 @@ interface MenuHeaderProps {
 }
 
 function MenuHeader({ onClose }: MenuHeaderProps) {
+  const [showNewChatDialog, setShowNewChatDialog] = useState<boolean>(false);
   return (
-    <div className="flex items-center gap-3 p-2">
-      <div className="h-full md:hidden">
-        <Button size="icon" variant="ghost" onClick={onClose}>
-          <X className="size-5" />
+    <>
+      <div className="flex items-center gap-3 p-2">
+        <div className="h-full md:hidden">
+          <Button size="icon" variant="ghost" onClick={onClose}>
+            <X className="size-5" />
+          </Button>
+        </div>
+        <h1 className="me-auto text-xl font-bold md:ms-2">Messages</h1>
+        <Button
+          size="icon"
+          variant="ghost"
+          title="Start new chat"
+          onClick={() => setShowNewChatDialog(true)}
+        >
+          <MailPlus className="size-5" />
         </Button>
       </div>
-    </div>
+      {showNewChatDialog && (
+        <NewChatDialog
+          onOpenChange={setShowNewChatDialog}
+          onChatCreated={() => {
+            setShowNewChatDialog(false);
+            onClose();
+          }}
+        />
+      )}
+    </>
   );
 }
