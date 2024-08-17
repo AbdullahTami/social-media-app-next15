066d3929cd579f2cb7555ddd990c510758ccbbac
abdullahqayid <3boo0odq0v@gmail.com>
2024-08-17 20:20:37 +0300

    google auth feature done

--- src/app/(auth)/login/google/GoogleSignInButton.tsx
+++ src/app/(auth)/login/google/GoogleSignInButton.tsx
@@ -9,10 +9,10 @@ export default function GoogleSignInButton() {
       className="bg-white text-black hover:bg-gray-100 hover:text-black"
       asChild
     >
-      <Link href="/login/google" className="flex w-full items-center gap-2">
+      <a href="/login/google" className="flex w-full items-center gap-2">
         <GoogleIcon />
         Sign in with Google
-      </Link>
+      </a>
     </Button>
   );
 }
